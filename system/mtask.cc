bb5122456ca3a948c961158642f451b167fceda0
Michal Machin <ltwood@gmail.com>
2012-10-22 17:45:40 +0300

    Добавлен модуль mtask, содержащий высокоуровневые примитивы, связанные с многопоточностью.
    Пока что этот модуль просто включает модуль mthread, что гарантирует формальную
    корректность старых программ, использующих модуль mtask.

---
+++ system/mtask.cc
@@ -0,0 +1,8 @@
+#include "mtask.h"
+
+/*#build_stop*/
+
+namespace lwml {
+
+
+}; // namespace lwml
---
+++ system/mtask.h
@@ -0,0 +1,18 @@
+// Высокоуровневые примитивы, связанные с многопоточностью.
+// lwml, (c) ltwood
+
+#ifndef _MTASK_
+#define _MTASK_
+
+#include "defs.h"
+#include "mdefs.h"
+
+#include "mthread.h"
+
+/*#build_stop*/
+
+namespace lwml {
+
+}; // namespace lwml
+
+#endif // _MTASK_
