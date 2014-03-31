b5e88270a720472926d6afcd365898e4b7dedd43
Michal Machin <ltwood@gmail.com>
2014-04-01 01:13:33 +0400

    Added new multitimer.

--- utils/timer.h
+++ utils/timer.h
@@ -40,6 +40,41 @@ private:
   double _start;
 };
 
+// Таймер многократного запуска.
+//
+// Такой таймер можно многократно запускать и останавливать,
+// получая в любой момент количество завершенных запусков,
+// суммарное время на все запуски и среднее время на один запуск.
+
+class mtimer : public value {
+public:
+  mtimer() : _num(0), _sum(0.0) {}
+
+  void start() { _tm.restart(); }
+
+  // возвращает время, прошедшее с момента старта
+  double stop() {
+    double left = _tm.left();
+    _sum += left;
+    _num++;
+    return left;
+  }
+
+  double sum() const { return _sum; }
+    // полное время на все завершенные запуски
+  int num() const { return _num; }
+    // количество завершенных запусков
+  double mid() const { return _sum / _num; }
+    // среднее время одного полного запуска
+  double res() const { return _tm.res(); }
+    // разрешение базового таймера
+
+private:
+  timer _tm;
+  int _num;
+  double _sum;
+};
+
 }; // namespace lwml
 
 #endif // _TIMERS_
