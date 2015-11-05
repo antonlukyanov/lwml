ea29656f77aceaeca7823a3d14a9e2f86b1aa040
Anton Lukyanov <anton.lukyanov@gmail.com>
2015-11-05 16:59:29 +0300

    Fixed export macros.

--- platforms.h
+++ platforms.h
@@ -19,26 +19,22 @@
   #define OS_LINUX 1
 #endif
 
-#ifdef __GNUC__
-
+#if OS_WIN
   #define LWML_EXPORT extern "C"     __attribute__((dllexport))
   #define LWML_IMPORT extern "C"     __attribute__((dllimport))
 
   #define UNUSED                     __attribute__((unused))
   #define PACKED                     __attribute__((packed))
-  #define PRINTF( fmt_idx, arg_idx ) __attribute__ ((format(printf, fmt_idx, arg_idx)));
+  #define PRINTF( fmt_idx, arg_idx ) __attribute__((format(printf, fmt_idx, arg_idx)));
   #define DEPRECATED                 __attribute__((deprecated))
-
 #else
-
-  #define LWML_EXPORT extern "C"     __declspec(dllexport)
-  #define LWML_IMPORT extern "C"     __declspec(dllimport)
+  #define LWML_EXPORT extern "C"     __attribute__((visibility("default")))
+  #define LWML_IMPORT extern "C"     __attribute__((visibility("hidden")))
 
   #define UNUSED 
   #define PACKED
-  #define PRINTF( fmt_idx, arg_idx )
+  #define PRINTF( fmt_idx, arg_idx ) __attribute__((format(printf, fmt_idx, arg_idx)));
   #define DEPRECATED                 __declspec(deprecated)
-
 #endif
 
 #endif // _PLATFORMS_
