538bd429a4af73d16e89a8cdf07e6828d284c1f7
Olya Dunaeva <Olaydy@gmail.com>
2019-01-14 11:47:20 +0300

    To filler added function mark_8n, for 8-neighbours connectivity.

--- c_alg/filler.h
+++ c_alg/filler.h
@@ -25,6 +25,9 @@ class filler : public scope {
 public:
   template<typename T, typename elT>
   static void mark_4n( T& bim, int x, int y, elT mark );
+  
+  template<typename T, typename elT>
+  static void mark_8n( T& bim, int x, int y, elT mark );
 };
 
 // implementation
@@ -48,6 +51,37 @@ void filler::mark_4n( T& bim, int x, int y, elT mark ){
       stack.put(int_point(p.x(), p.y()-1));
     if( p.y() < ly-1 && bim(p.y()+1, p.x()) == what )
       stack.put(int_point(p.x(), p.y()+1));
+  }
+}
+
+template<typename T, typename elT>
+void filler::mark_8n( T& bim, int x, int y, elT mark ){
+  t_list<int_point> stack(t_list<int_point>::MANUAL_PACK);
+
+  int lx = bim.col();
+  int ly = bim.str();
+  elT what = bim(y, x);
+  stack.put(int_point(x, y));
+  while( !stack.is_empty() ){
+    int_point p = stack.get();
+    bim(p.y(), p.x()) = mark;
+    if( p.x() > 0 && bim(p.y(), p.x()-1) == what )
+      stack.put(int_point(p.x()-1, p.y()));
+    if( p.x() < lx-1 && bim(p.y(), p.x()+1) == what )
+      stack.put(int_point(p.x()+1, p.y()));
+    if( p.y() > 0 && bim(p.y()-1, p.x()) == what )
+      stack.put(int_point(p.x(), p.y()-1));
+    if( p.y() < ly-1 && bim(p.y()+1, p.x()) == what )
+      stack.put(int_point(p.x(), p.y()+1));
+      
+    if( p.x() > 0 && p.y() > 0 && bim(p.y()-1, p.x()-1) == what )
+      stack.put(int_point(p.x() - 1, p.y() - 1));
+    if( p.x() < lx-1 && p.y() > 0 && bim(p.y()-1, p.x()+1) == what )
+      stack.put(int_point(p.x() + 1, p.y() - 1));
+    if( p.x() > 0 && p.y() < ly-1 && bim(p.y()+1, p.x()-1) == what )
+      stack.put(int_point(p.x() - 1, p.y() + 1));
+    if( p.x() < lx-1 && p.y() < ly-1 && bim(p.y()+1, p.x()+1) == what )
+      stack.put(int_point(p.x() + 1, p.y() + 1));    
   }
 }
 
